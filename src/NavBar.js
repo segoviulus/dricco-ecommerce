ba2dc50ee399c223c5292937ced627b94717dc8e
Ariel Segovia <segovia.a.ariel@gmail.com>
2022-05-26 12:16:58 -0300

    App React

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+// Fragmento <> </> para no utilizar etiquetas
+
+const NavBar = () => {
+return (
+   <header>
+       <h1>Titulo</h1>
+        <nav>
+            <a href="#">Celulares</a>
+            <a href="#">Tablets</a>
+            <a href="#">Televisores</a>
+        </nav>
+   </header>
+)
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+// librerias de React, la trae desde node_modules
+//import NombreComponente from 'NombreComponente';
 import React from 'react';
+
+//libreria de ReactDOM, la trae desde node_modules
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//Agarrar una app que ya exista en React 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*function App() {
+  return "Hola Mundo desde React";
+}*/
+
+// const App = () => "Hola Mundo desde React";
+
+import "./estilos.css"
+
+//Mostrar la App en el DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
